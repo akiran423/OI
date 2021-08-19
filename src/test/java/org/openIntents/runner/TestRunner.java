60e2bde99e393d6bd4c1ca07afb98a6dc9828aaf
akiran423 <akiran423@gmail.com>
2021-08-19 15:58:32 +0530

    Project for OpenIntents shopping mobile automation

---
+++ src/test/java/org/openIntents/runner/TestRunner.java
@@ -0,0 +1,17 @@
+package org.openIntents.runner;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(features = "features",glue={"org.openIntents.steps"} )
+
+public class TestRunner {
+
+	@AfterClass
+	public static void writeExtentReport() {
+	}
+}
