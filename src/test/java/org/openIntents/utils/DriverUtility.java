5bd7affa8cdf5e5f3e3a50b4569e3ef6c9a05df7
akiran423 <akiran423@gmail.com>
2021-08-20 12:45:11 +0530

    comments update

--- src/test/java/org/openIntents/utils/DriverUtility.java
+++ src/test/java/org/openIntents/utils/DriverUtility.java
@@ -1,10 +1,8 @@
 package org.openIntents.utils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
 
@@ -12,42 +10,45 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class DriverUtility {
 	Properties props;
-	/**
- 	* Method to invoke Android driver
-	* with configured app in oi.properties
- 	*/
-	public AndroidDriver<AndroidElement> getDriver() throws IOException {
-		DesiredCapabilities cap=getDesiredCapabilities();
-		AndroidDriver<AndroidElement> driver=new AndroidDriver<AndroidElement>(new URL(props.getProperty("url")),cap);
-		return driver;
-		
-	}
-	
-	/**
- 	* Method to get desired capabilities 
-	* as configured in oi.properties
- 	*/
-	public DesiredCapabilities getDesiredCapabilities() throws IOException {
-		FileReader reader=new FileReader("src/test/resources/oi.properties");  
-	      
-		props=new Properties();  
-		props.load(reader);  
 
-		File appDir=new File(props.getProperty("appDirectory"));
-		File app=new File(appDir,props.getProperty("appName"));
-		DesiredCapabilities cap=new DesiredCapabilities();
+	/**
+	 * Method to invoke Android driver with configured app in oi.properties
+	 */
+	public AndroidDriver<AndroidElement> getDriver() throws IOException {
+		DesiredCapabilities cap = getDesiredCapabilities();
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL(props.getProperty("url")),
+				cap);
+		return driver;
+
+	}
+
+	/**
+	 * Method to set desired capabilities , and return to driver as configured in
+	 * oi.properties
+	 */
+	public DesiredCapabilities getDesiredCapabilities() throws IOException {
+		FileReader reader = new FileReader("src/test/resources/oi.properties");
+
+		props = new Properties();
+		props.load(reader);
+
+		File appDir = new File(props.getProperty("appDirectory"));
+		File app = new File(appDir, props.getProperty("appName"));
+		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, props.getProperty("deviceName"));
-		//cap.setCapability(MobileCapabilityType.DEVICE_NAME, "KiranPixel5");
-		
 		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, props.getProperty("automationName"));
-		
+		cap.setCapability(MobileCapabilityType.PLATFORM, props.getProperty("platform"));
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, props.getProperty("platformVersion"));
+		// cap.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"org.openintents.shopping.ui.LayoutChoiceActivity");
+		// cap.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,"org.openintents.shopping");
 		return cap;
-		
+
 	}
 
 }
