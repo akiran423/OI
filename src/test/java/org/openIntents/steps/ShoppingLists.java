c85d5a10a35f8eef4f28fb7db10445d7a5a396bf
akiran423 <akiran423@gmail.com>
2021-08-23 10:24:44 +0530

    Locator update

--- src/test/java/org/openIntents/steps/ShoppingLists.java
+++ src/test/java/org/openIntents/steps/ShoppingLists.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.collections.bag.SynchronizedSortedBag;
 import org.apache.log4j.*;
 import org.openIntents.utils.DriverUtility;
 import org.openIntents.utils.GeneralUtility;
@@ -21,6 +23,15 @@ public class ShoppingLists {
 
 	AndroidDriver<AndroidElement> driver;
 	Logger log = LogManager.getLogger(ShoppingLists.class);
+	
+	List<String> items1 = new ArrayList<String>();
+	
+	public ShoppingLists()
+	{
+		items1.add("Pencil");
+		items1.add("Book");
+		items1.add("Pen");
+	}
 
 	@Given("^Open OI application$")
 	public void Open_OI_application() throws IOException {
@@ -36,7 +47,7 @@ public class ShoppingLists {
 		log.info(" Open_OI_application is successfully executed");
 	}
 
-	@When("^create new list$")
+	@When("^Create new list$")
 	public void create_new_list() {
 		log.info("create_new_list Execution started");
 		// click on shopping list menu
@@ -55,9 +66,7 @@ public class ShoppingLists {
 	@When("^Add few items$")
 	public void Add_few_items() {
 		log.info("Add_few_items Execution started");
-		List<String> items1 = new ArrayList<String>();
-		items1.add("Book");
-		items1.add("Pen");
+		
 		addItemsToNewList(items1);
 
 		log.info("Add_few_items is successfully executed");
@@ -77,33 +86,40 @@ public class ShoppingLists {
 		log.info("addItemsToNewList is successfully executed");
 	}
 
-	@When("^delete one item$")
+	@When("^Delete one item$")
 	public void delete_one_item() {
 		log.info("delete_one_item Execution started");
-		driver.findElement(By.id("org.openintents.shopping:id/check")).click();
-
+		//driver.findElementsByAndroidUIAutomator("new UiSelector().checked(false)").get(8).click();
+		driver.findElementsByClassName("android.widget.CheckBox").get(0).click();
 		log.info("delete_one_item is successfully executed");
 	}
 
-	@Then("^only added elements should retain$")
+	@Then("^Only added elements should retain$")
 	public void only_added_elements_should_retain() {
 		log.info("only_added_elements_should_retain Execution started");
-		assertTrue(driver.findElement(By.xpath("//*[@text()='Pen']")).isDisplayed());
+		assertTrue(driver.findElements(By.id("org.openintents.shopping:id/name")).get(0).isDisplayed());
 		log.info("only_added_elements_should_retain is successfully executed");
 	}
 
-	@When("^sort the list$")
+	@When("^Sort the list$")
 	public void sort_the_list() {
 		log.info("sort_the_list Execution started");
 		driver.findElement(By.id("org.openintents.shopping:id/title")).click();
 		log.info("sort_the_list is successfully executed");
 	}
 
-	@Then("^verify sorted list items shown$")
-	public void verify_sorted_list_items_shown() {
-		log.info("verify_sorted_list_items_shown Execution started");
-		assertTrue(driver.findElement(By.xpath("//*[@text()='Pen']")).isDisplayed());
-		log.info("verify_sorted_list_items_shown is successfully executed");
+	@Then("^List items are in sorted order$")
+	public void list_items_are_in_sorted_order() {
+		log.info("list_items_are_in_sorted_order Execution started");
+		//sort the items
+		Collections.sort(items1);
+		int size=driver.findElements(By.xpath("//android.widget.TextView[@resource-id='org.openintents.shopping:id/name']")).size();
+		//verify items are in sorted order
+		for(int i=0;i<(size-1);i++)
+		{
+			assertTrue(driver.findElements(By.xpath("//android.widget.TextView[@resource-id='org.openintents.shopping:id/name']")).get(i).getText().equals(items1.get(i)));
+		}
+		log.info("list_items_are_in_sorted_order is successfully executed");
 	}
 
 }
